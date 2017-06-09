9d6785ac3640d1f0d77db263c3a28cc46cf5e392
Eduardo Santoro Morgan <exs416@case.edu>
2017-06-08 22:04:43 -0300

    Some work on 8_13

--- lista7_c8_13.m
+++ lista7_c8_13.m
@@ -1,18 +1,15 @@
 N = 100;
-k = 3;
 
 n = linspace(1, 100, N);
-omega = (pi - (-pi)).*rand(1, 100) - pi
+omega = pi*(2*rand -1);
 
 omega1 = 0.4*pi;
 omega2 = 0.5*pi;
 omega3 = 1.2*pi;
 
 %gera o vetor x
-x = 2*exp(1i*(omega + n*omega1)) + 2*exp(1i*(omega + n*omega2)) + 2*exp(1i*(omega + n*omega3)) + 0.01 * randn(1, N);
+x = 2*exp(1i*omega) * (exp(1i*n*omega1) + exp(1i*n*omega2) + exp(1i*n*omega3)) + randn(1, N);
 
-%calculo da autocorrelacao
-Rx = toeplitz(acf((x', 6));
 
 [v, d] = eig(Rx);
 
@@ -21,10 +18,10 @@ eigValues = diag(d);
 
 
 %%
-X = zeros(20, 100)
+X = zeros(20, 100);
 for k = 1: 20
-    omega = (pi - (-pi)).*rand(1, 100) - pi
-    x = 2*exp(1i*(omega + n*omega1)) + 2*exp(1i*(omega + n*omega2)) + 2*exp(1i*(omega + n*omega3)) + 0.01 * randn(1, N);
+    omega = pi*(2*rand -1);
+    x = 2*exp(1i*omega) * (exp(1i*n*omega1) + exp(1i*n*omega2) + exp(1i*n*omega3)) + randn(1, N);
     X(k, :) = x;
 end
 
