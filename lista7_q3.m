7fb43e10d11add19ebff403a4f415688221f3e57
Eduardo Santoro Morgan <exs416@case.edu>
2017-06-10 16:05:37 -0300

    Still not working

--- lista7_q3.m
+++ lista7_q3.m
@@ -4,14 +4,15 @@ n = linspace(1, 10000, 1);
 w1 = pi/2;
 w2 = 1.1 * pi/2;
 
-fi = linspace(0, 2*pi, N);
+fi1 = 2*pi*rand;
+fi2 = 2*pi*rand;
 
 w = randn(1, N);
 wfiltered = filter([1, 2, 0, -2, -1], [1], w);
 
-x = 2 * cos(n*w1 + fi) + 2 * cos(n*w2 + fi) + wfiltered;
+x = 2 * cos(n*w1 + fi1) + 2 * cos(n*w2 + fi2) + wfiltered;
 
-rx = acf(x', 255);
+rx = autocorr(x, 21);
 fftSize = 256;
 spec = fft(rx, fftSize);
 omega = 2*pi * (1:(fftSize)) / fftSize;
@@ -43,6 +44,16 @@ powerW = sum(spectroW) / N;
 
 
 %%
-%
-Pw = abs(fft(acf(wfiltered', 21), N));
+%d)
 
+N = 256;
+n = linspace(1, 10000, 1);
+w1 = pi/2;
+w2 = 1.1 * pi/2;
+
+fi = linspace(0, 2*pi, N);
+
+w = randn(1, N);
+wfiltered = filter([1, 2, 0, -2, -1], [1], w);
+
+x = 2 * cos(n*w1 + fi) + 2 * cos(n*w2 + fi) + wfiltered;
